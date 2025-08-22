a0482278f7e0fbd575f4987c3c05a0150e57bb09
Fsehaye Medhanie <werkat41@gmail.com>
2025-08-22 01:48:16 -0700

    finished the lab-1 q-1

--- week01/lab01/q1.py
+++ week01/lab01/q1.py
@@ -1,3 +1,12 @@
+
+'''
+Question 1:
+Write a Python program that asks for a string as input and prints a version with each word capitalized and a full stop
+added at the end (if not already present). Constriants: Use character-by-character processing and string concatenation, avoiding slicing 
+and string methods like upper(), title() and endswith().
+
+'''
+
 # print the program title 
 print("Question 1: Capitalizing Each word")
 
@@ -9,13 +18,19 @@ str_input = input("Please enter a sentence:")
 
 result = ""
 pre = " "
-for ch in str_input:
+for i, ch in enumerate(str_input):
    if pre == " " and ch != " ": 
       result +=ch.capitalize()
    else:
        result += ch
    pre = ch
    
+   if i == len(str_input)-1 and ch != ".":
+       result += "."
+   
+   # if ch == str_input[-1] and ch != ".":  
+   #    result +="." 
+     
 # print the result string
 print("The output is", result)    
     
