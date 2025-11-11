67bed5e474ee4bfcc0fb4913f65c11501f3d14bf
Fsehaye Medhanie <werkat41@gmail.com>
2025-11-10 21:08:15 -0800

    update for the last activities

--- week11/myPractice-Docorators/cofee_shope_example.py
+++ week11/myPractice-Docorators/cofee_shope_example.py
@@ -37,12 +37,79 @@ class DarkRoast(Beverage):
     def get_description(self) -> str:
         return "Dark Rost Coffee"
     
-    def cost(self)-> float:
+    def cost(self)-> float:        
         return 0.99
     
-    
+# create the decorator base class
+  
 class CondimentDecorator(Beverage):
-    pass
+    def __init__(self, beverage: Beverage):
+        self.__beverage = beverage
+    
+    @property
+    def beverage(self):
+        return self.__beverage
+        
+    @beverage.setter
+    def beverage(self, beverage):
+        self.__beverage = beverage
+    @abstractmethod
+    def get_description(self) -> str:
+        pass
+    
+    
+class Milk(CondimentDecorator):
+    def get_description(self) -> str:
+        return self.beverage.get_description() + ", Milk"
+    
+    def cost(self) -> float:
+        return self.beverage.cost() + 0.50
+    
+class Mocha(CondimentDecorator):
+    def get_description(self) -> str:
+        return self.beverage.get_description() + ", Mocha"
+    
+    def cost(self) -> float:
+        return self.beverage.cost() + 0.70
+    
+class Whip(CondimentDecorator):
+    
+    def get_description(self) -> str:
+        return self.beverage.get_description() + ", Soy"
+    
+    def cost(self) -> float:
+        return self.beverage.cost() + 0.45
+    
+class Caramel(CondimentDecorator):
+    
+    def get_description(self) -> str:
+        return self.beverage.get_description() + ", Caramel"
+    
+    def cost(self) -> float:
+        return self.beverage.cost() + 0.55
+    
+
+
+def main():
+    
+    print("\n" + "=" * 50)
+    print("Wlcome to the coffee shop!")
+    print("=" *50 + "\n")
+    
+    # order simple Espreso
+    print("Order 1: Plain Espresso")
+    beverage1 = Espresso()
+    beverage1.display()  
+    
+    # order2 : Dark Roast with Milk and Mocha 
+    print("\nOrder 2: Dark Roast with Milk and Mocha")
+    beverage2 = DarkRoast()
+    beverage2 = Milk(beverage2)
+    beverage2 = Mocha(beverage2)
+    beverage2.display()
+    
+if __name__ == "__main__":
+    main() 
 
 
         
